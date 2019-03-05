ad9b4059e86947b372974b99ba60f40013b5957c
V School <learn@vschool.io>
2019-03-05 13:35:59 -0700

    Update app.js

--- submit/app.js
+++ submit/app.js
@@ -1,3 +1,3 @@
-// The "submit" event refreshed your brower by default
+// The "submit" event refreshes your brower by default
 // use "e.preventDefault" in the first line of your callback function to prevent this
 // remember to pass "e" into the callback function
